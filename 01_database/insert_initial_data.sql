854ea95458df2f52ca4a16e3417988fcfd8700d6
Debora Cupertino <162383564+coffeeinada@users.noreply.github.com>
2026-09-16 16:32:02 -0300

    Create insert_initial_data.sql

---
+++ 01_database/insert_initial_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO DimEmpresa (EmpresaID, Ticker, NomeEmpresa, Setor) VALUES
+(1, 'VALE3', 'Vale S.A.', 'Mineração'),
+(2, 'PETR4', 'Petróleo Brasileiro S.A. - Petrobras', 'Petróleo e Gás'),
+(3, 'WEGE3', 'WEG S.A.', 'Bens de Capital'),
+(4, 'ABEV3', 'Ambev S.A.', 'Bebidas'),
+(5, 'RENT3', 'Localiza Rent a Car S.A.', 'Serviços / Aluguel de Carros');
